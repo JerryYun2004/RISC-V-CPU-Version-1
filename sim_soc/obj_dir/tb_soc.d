fb7e6ba350bb12300ef309cfdac37568cf0e89c7
Jerry Yun <zizhuoyun@gmail.com>
2026-06-02 23:14:43 +0100

    Ignore generated SoC simulation build outputs

--- sim_soc/obj_dir/tb_soc.d
+++
@@ -1,6 +0,0 @@
-tb_soc.o: ../tb_soc.cpp Vsoc_top.h \
-  /opt/homebrew/Cellar/verilator/5.048/share/verilator/include/verilated.h \
-  /opt/homebrew/Cellar/verilator/5.048/share/verilator/include/verilated_config.h \
-  /opt/homebrew/Cellar/verilator/5.048/share/verilator/include/verilatedos.h \
-  /opt/homebrew/Cellar/verilator/5.048/share/verilator/include/verilated_types.h \
-  /opt/homebrew/Cellar/verilator/5.048/share/verilator/include/verilated_funcs.h
